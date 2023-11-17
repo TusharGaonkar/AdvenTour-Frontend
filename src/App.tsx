c6c2db1b1e243b05014a8f6301e5ebeafd50f27a
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-17 21:28:18 +0530

    sign in and register routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { NextUIProvider } from '@nextui-org/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import LoginForm from './pages/loginForm';
+import SignupForm from './pages/SignUpForm';
 
 function App() {
   return (
     <NextUIProvider>
-      <LoginForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<SignupForm />} />
+        </Routes>
+      </BrowserRouter>
     </NextUIProvider>
   );
 }
