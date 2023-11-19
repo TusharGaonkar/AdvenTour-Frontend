ecc8de7ac47af4fae5a0c59f4c467e9e700bc44d
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-19 20:44:54 +0530

    adventour landing page initial setup

---
+++ src/pages/AdventourLandingPage.tsx
@@ -0,0 +1,10 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
+import NavBar from '../features/AdventourLandingPage/Navbar';
+import MainCover from '../features/AdventourLandingPage/MainCover';
+const AdventourLandingPage = () => (
+  <>
+    <NavBar />
+    <MainCover />
+  </>
+);
+export default AdventourLandingPage;
