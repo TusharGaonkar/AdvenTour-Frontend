49825127a576783456117ff066b37fd97d591fa5
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-19 20:45:17 +0530

    Tour page initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import LoginForm from './pages/loginForm';
 import SignupForm from './pages/SignUpForm';
+import AdventourLandingPage from './pages/AdventourLandingPage';
+import ToursPage from './pages/ToursPage';
 
 function App() {
   return (
     <NextUIProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<AdventourLandingPage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<SignupForm />} />
+          <Route path="/tours" element={<ToursPage />} />
         </Routes>
       </BrowserRouter>
     </NextUIProvider>
---
+++ src/pages/ToursPage.tsx
@@ -0,0 +1,8 @@
+import NavBar from '../features/AdventourLandingPage/Navbar';
+
+const ToursPage = () => (
+  <>
+    <NavBar />
+  </>
+);
+export default ToursPage;
