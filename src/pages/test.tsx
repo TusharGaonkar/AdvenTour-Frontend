a57c6ff02ed09dad948feaf653ca6301ec2e605e
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-22 15:07:51 +0530

    sample server response for the layout

---
+++ src/pages/test.tsx
@@ -0,0 +1,125 @@
+// eslint-disable-next-line import/prefer-default-export
+export const tour = {
+  title: 'Rishikesh Adventure Tour',
+  description:
+    'Rishikesh is a popular destination for adventure sports like white river rafting, bungee jumping, and trekking. This 3-day tour takes you to some of the best adventure spots in Rishikesh, and gives you a chance to experience the thrill of these activities.',
+  mainCoverImage:
+    'https://www.tripadvisor.in/AttractionProductReview-g297669-d17547733-Rishikesh_Adventure_Tour_Package-Rishikesh_Dehradun_District_Uttarakhand.html',
+  additionalImages: [
+    'https://www.tripadvisor.in/AttractionProductReview-g297669-d17547733-Rishikesh_Adventure_Tour_Package-Rishikesh_Dehradun_District_Uttarakhand.html',
+  ],
+  tourLocation: {
+    type: 'Point',
+    coordinates: [30.1043, 78.2856],
+  },
+  createdBy: '123586858483',
+  tourDurationInDays: 3,
+  tourStartDates: ['2023-11-01', '2023-11-08', '2023-11-15'],
+  repeatsEachMonth: false,
+  tourStartTime: '9:00 AM',
+  priceInRupees: 20000,
+  discountInRupees: 4000,
+  ageGroups: {
+    minAge: 16,
+    maxAge: 60,
+  },
+  tourMaxCapacity: 30,
+  maxPeoplePerBooking: 3,
+  tourDifficulty: 'Medium',
+  liveGuideLanguages: ['English', 'Hindi'],
+  whatsIncluded: ['Accommodation', 'Food', 'Transportation', 'Activities'],
+  whatsNotIncluded: ['Personal expenses', 'Travel insurance'],
+  additionalInformation:
+    'This tour is suitable for people of all fitness levels. However, it is important to note that some of the activities, such as white river rafting and trekking, can be physically demanding.',
+  cancellationPolicy:
+    'Full refund if cancelled 30 days or more in advance. 50% refund if cancelled 15-29 days in advance. No refund if cancelled less than 14 days in advance.',
+  FAQ: [
+    {
+      question: 'What is the difficulty level of this tour?',
+      answer:
+        'This tour is rated as medium difficulty. There is some hiking and walking involved, but the terrain is not too challenging. However, it is important to note that some of the activities, such as white river rafting and bungee jumping, can be physically demanding.',
+    },
+    {
+      question: 'What is the weather like in Rishikesh?',
+      answer:
+        'The weather in Rishikesh varies depending on the time of year. In the summer, the weather is hot and humid, with occasional thunderstorms. In the winter, the weather is mild and sunny. The best time to visit Rishikesh is during the shoulder seasons of spring and autumn, when the weather is pleasant.',
+    },
+  ],
+  itinerary: [
+    {
+      day: 1,
+      description:
+        'Arrival in Rishikesh. Check into your hotel and rest for the day. In the evening, visit the Lakshman Jhula and Triveni Ghat.',
+      activities: [
+        {
+          activityName: 'Water Rafting',
+          place: 'Lakshman Jhula',
+          location: {
+            type: 'Point',
+            coordinates: [30.1043, 78.2856],
+          },
+          image:
+            'https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2022/12/15180616/Adventure-Destinations-In-India-1.jpg?tr=w-1200,h-900',
+        },
+      ],
+      foodIncluded: 'Breakfast, Lunch, & Dinner',
+      accommodationIncluded: 'Hotel Ganges',
+    },
+    {
+      day: 2,
+      description: 'Engage in adventurous activities and explore some iconic spots.',
+      activities: [
+        {
+          activityName: 'Bungee Jumping',
+          place: 'Jumpin Heights',
+          location: {
+            type: 'Point',
+            coordinates: [30.2155, 78.2798],
+          },
+          image:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdO6zYI0IPa0z9yycPBs41LHYvpZoSjUut0w&usqp=CAU',
+        },
+        {
+          activityName: 'Zip Lining',
+          place: 'Neem Beach',
+          location: {
+            type: 'Point',
+            coordinates: [30.1167, 78.3333],
+          },
+          image:
+            'https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2022/12/15180616/Adventure-Destinations-In-India-1.jpg?tr=w-1200,h-900',
+        },
+      ],
+      foodIncluded: 'Breakfast, Lunch, & Dinner',
+      accommodationIncluded: 'Hotel Ganges',
+    },
+    {
+      day: 3,
+      description: 'Continue the adventure with thrilling activities and exploration.',
+      activities: [
+        {
+          activityName: 'Trekking',
+          place: 'Neer Garh Waterfall',
+          location: {
+            type: 'Point',
+            coordinates: [30.0869, 78.2794],
+          },
+          image:
+            'https://ihplb.b-cdn.net/wp-content/uploads/2014/06/Parasailing-and-Paragliding-in-Goa.jpg',
+        },
+        {
+          activityName: 'Rock Climbing',
+          place: 'Shivpuri',
+          location: {
+            type: 'Point',
+            coordinates: [30.2197, 78.3217],
+          },
+          image:
+            'https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2022/12/15180616/Adventure-Destinations-In-India-1.jpg?tr=w-1200,h-900',
+        },
+      ],
+      foodIncluded: 'Breakfast, Lunch',
+      accommodationIncluded: 'Hotel Ganges',
+    },
+  ],
+};
