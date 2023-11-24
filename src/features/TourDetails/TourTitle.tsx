229695ded0915d18f6368b900573886669b5f799
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-24 13:30:47 +0530

    Tour title component initial layout

---
+++ src/features/TourDetails/TourTitle.tsx
@@ -0,0 +1,24 @@
+import { MdOutlineRecommend } from 'react-icons/md';
+import StarRating from 'react-star-ratings';
+import { tour } from '../../pages/test';
+
+const TourTitle = () => {
+  return (
+    <div className="flex flex-col items-start gap-3">
+      <h1 className="text-3xl font-semibold">{tour.title}</h1>
+      <p className="text-sm underline text-slate-400">By Tushar Gaonkar</p>
+
+      <div className="flex items-center justify-center gap-2">
+        <StarRating rating={4.5} starRatedColor="orange" starDimension="20px" starSpacing="3px" />
+        <p className="text-sm font-semibold text-slate-600">(4.5/5)</p>
+        <p className="text-sm underline">103 Reviews</p>
+        <div className="flex items-center gap-1">
+          <MdOutlineRecommend className="text-xl text-red-500" />
+          <p className="text-sm underline">Recommended by 10 users</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TourTitle;
