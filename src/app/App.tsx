f7dd98612480f6ce3cf6a263475f3f555705858a
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-26 23:14:27 +0530

    moved App component inside app folder

---
+++ src/app/App.tsx
@@ -0,0 +1,32 @@
+import { NextUIProvider } from '@nextui-org/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import store from './store';
+import LoginForm from '../pages/loginForm';
+import SignupForm from '../pages/SignUpForm';
+import AdventourLandingPage from '../pages/AdventourLandingPage';
+import ToursPage from '../pages/ToursPage';
+import TourDetailsPage from '../pages/TourDetailsPage';
+import '../index.css';
+
+function App() {
+  return (
+    <NextUIProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AdventourLandingPage />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<SignupForm />} />
+            <Route path="/tours" element={<ToursPage />} />
+            <Route path="/tours/:id" element={<TourDetailsPage />} />
+          </Routes>
+          <Toaster position="top-right" reverseOrder={false} />
+        </BrowserRouter>
+      </Provider>
+    </NextUIProvider>
+  );
+}
+
+export default App;
