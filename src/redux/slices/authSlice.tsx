bd1a4b3647e24c20923c42d6e2f19710a46da8bf
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-26 23:16:41 +0530

    api slice for authentication , injects endpoints to the main api slice

---
+++ src/redux/slices/authSlice.tsx
@@ -0,0 +1,50 @@
+import apiSlice from './apiSlice';
+
+export interface LoginResponse {
+  status: string;
+  data: Data;
+}
+interface Data {
+  user: User;
+  token: string;
+}
+export interface User {
+  _id: string;
+  userName: string;
+  email: string;
+  authProvider: string;
+  role: string;
+  __v: number;
+}
+
+type UserCredentialsType = {
+  email: string;
+  password: string;
+  confirmPassword?: string;
+};
+
+const authSlice = apiSlice.injectEndpoints({
+  endpoints: (build) => ({
+    loginUser: build.mutation<LoginResponse, UserCredentialsType>({
+      query: (userCredentials) => ({
+        url: '/auth/login',
+        method: 'POST',
+        body: userCredentials,
+      }),
+    }),
+    registerUser: build.mutation<
+      LoginResponse,
+      Required<Pick<UserCredentialsType, 'email' | 'password' | 'confirmPassword'>>
+    >({
+      query: (userCredentials) => ({
+        url: '/auth/register',
+        method: 'POST',
+        body: userCredentials,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginUserMutation, useRegisterUserMutation } = authSlice;
+
+export default authSlice;
