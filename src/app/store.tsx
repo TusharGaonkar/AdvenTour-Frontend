f1c95a3e3b444c629972e9d720036d957e61edc5
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:42:46 +0530

    filter tours slice configured in the store

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import apiSlice from '../redux/slices/apiSlice';
 import userSlice from '../redux/slices/userSlice';
+import filterToursSlice from '../redux/slices/filterToursSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     [userSlice.name]: userSlice.reducer,
+    [filterToursSlice.name]: filterToursSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
