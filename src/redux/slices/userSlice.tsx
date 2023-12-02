00c56f82644608959978bf2cec7886a378cec9e9
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:56:38 +0530

    redux async thunk updated to re-authenticate user when the app mounts .. changes the usersInfo based on the jwt token

--- src/redux/slices/userSlice.tsx
+++ src/redux/slices/userSlice.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 type UserSliceInitialStateType = {
   user: {
@@ -7,9 +7,9 @@ type UserSliceInitialStateType = {
     userName: string;
     avatar?: string;
     email: string;
-    role: 'admin' | 'user' | 'local-guide';
+    role?: 'admin' | 'user' | 'local-guide' | null;
   } | null;
-  token: string | null;
+  token?: string | null;
   isLoggedIn: boolean;
 };
 
@@ -18,6 +18,22 @@ const userSliceInitialState: UserSliceInitialStateType = {
   token: null,
   isLoggedIn: false,
 };
+
+export const reAuthenticate = createAsyncThunk(
+  'userInfo/reAuthenticate',
+  async (): Promise<unknown> => {
+    const response = await fetch('http://localhost:2000/api/v-1.0/auth/verifyToken', {
+      credentials: 'include',
+    });
+
+    if (response.ok) {
+      const user = await response.json();
+      return user;
+    }
+
+    throw new Error('Jwt integrity verification failed logging out...');
+  }
+);
 
 const userSlice = createSlice({
   name: 'userInfo',
@@ -35,6 +51,22 @@ const userSlice = createSlice({
       state.token = null;
       state.isLoggedIn = false;
     },
+  },
+
+  extraReducers: (build) => {
+    build
+      .addCase(reAuthenticate.fulfilled, (state, action) => {
+        const { user } = action.payload.data;
+        if (user) {
+          state.user = { ...user };
+          state.isLoggedIn = true;
+        }
+      })
+      .addCase(reAuthenticate.rejected, (state) => {
+        state.user = null;
+        state.token = null;
+        state.isLoggedIn = false;
+      });
   },
 });
 
