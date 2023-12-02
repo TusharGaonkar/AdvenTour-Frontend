bfe0e5b0ff04a54436161424a713df3eb10b8968
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:44:36 +0530

    added filter functionality to filters uses filter slice to dispatch events

--- src/features/Tours/Filters.tsx
+++ src/features/Tours/Filters.tsx
@@ -1,46 +1,180 @@
-import { Slider, Checkbox } from '@nextui-org/react';
+import { Slider, Checkbox, Radio, RadioGroup } from '@nextui-org/react';
 import StarRatings from 'react-star-ratings';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  setPrice,
+  setTourDifficulty,
+  setTourDuration,
+  setAgeGroup,
+  setTourRating,
+} from '../../redux/slices/filterToursSlice';
+import { useEffect } from 'react';
 
-const Filters = () => (
-  <div className="flex flex-col items-start h-full gap-6">
-    <div className="flex flex-col items-start gap-12">
+const Filters = () => {
+  const dispatch = useDispatch();
+  const filters = useSelector((state: any) => state.filterToursQueryString);
+
+  return (
+    <div className="flex flex-col items-start gap-8 p-4 rounded-xl">
       <p className="text-lg font-bold">Filters</p>
+
+      <div className="flex flex-col w-full gap-1">
+        <p className="font-semibold ">Trip difficulty</p>
+        <div className="flex flex-row gap-3">
+          <Checkbox
+            label="easy"
+            isSelected={filters.tourDifficulty.Easy}
+            onChange={() =>
+              dispatch(
+                setTourDifficulty({ difficulty: 'Easy', value: !filters.tourDifficulty.Easy })
+              )
+            }
+          >
+            Easy
+          </Checkbox>
+          <Checkbox
+            label="medium"
+            isSelected={filters.tourDifficulty.Medium}
+            onChange={() =>
+              dispatch(
+                setTourDifficulty({ difficulty: 'Medium', value: !filters.tourDifficulty.Medium })
+              )
+            }
+          >
+            Medium
+          </Checkbox>
+          <Checkbox
+            label="hard"
+            isSelected={filters.tourDifficulty.Hard}
+            onChange={() =>
+              dispatch(
+                setTourDifficulty({ difficulty: 'Hard', value: !filters.tourDifficulty.Hard })
+              )
+            }
+          >
+            Hard
+          </Checkbox>
+        </div>
+      </div>
+
       <div className="flex flex-col w-full gap-3">
-        <p className="font-semibold">Tour price per person</p>
+        <p className="font-semibold">Tour price per individual</p>
         <Slider
           label="Price"
           step={100}
-          minValue={0}
-          maxValue={100000}
-          defaultValue={[2500, 10000]}
+          minValue={700}
+          maxValue={3_00_000}
+          defaultValue={filters.priceInRupees}
           className="min-w-[250px]"
-          formatOptions={{ style: 'currency', currency: 'INR' }}
+          formatOptions={{ style: 'currency', currency: 'INR', minimumFractionDigits: 0 }}
+          onChange={(value) => dispatch(setPrice(value as number[]))}
         />
+      </div>
+      <div className="flex flex-col w-full gap-1">
+        <p className="font-semibold ">Trip duration</p>
+        <Checkbox
+          label="one day"
+          isSelected={filters.tourDurationInDays['1']}
+          onChange={() =>
+            dispatch(setTourDuration({ duration: '1', value: !filters.tourDurationInDays['1'] }))
+          }
+        >
+          1 day
+        </Checkbox>
+        <Checkbox
+          label="two days"
+          isSelected={filters.tourDurationInDays['2']}
+          onChange={() =>
+            dispatch(setTourDuration({ duration: '2', value: !filters.tourDurationInDays['2'] }))
+          }
+        >
+          2 days
+        </Checkbox>
+        <Checkbox
+          label="three days"
+          isSelected={filters.tourDurationInDays['3']}
+          onChange={() =>
+            dispatch(setTourDuration({ duration: '3', value: !filters.tourDurationInDays['3'] }))
+          }
+        >
+          3 days
+        </Checkbox>
+        <Checkbox
+          label="four or more"
+          isSelected={filters.tourDurationInDays['4']}
+          onChange={() =>
+            dispatch(setTourDuration({ duration: '4', value: !filters.tourDurationInDays['4'] }))
+          }
+        >
+          4 & more
+        </Checkbox>
       </div>
 
       <div className="flex flex-col w-full gap-1">
-        <p className="font-semibold "> Group age group</p>
-        <Checkbox label="Nearby">Children (0 - 12 years)</Checkbox>
-        <Checkbox label="Nearby">Teenagers (12 - 18 years)</Checkbox>
-        <Checkbox label="Nearby">Adults (18 - 64 years)</Checkbox>
-        <Checkbox label="Nearby">Seniors(65+ years)</Checkbox>
+        <p className="font-semibold "> Allowed age groups</p>
+        <Checkbox
+          label="children"
+          isSelected={filters.ageGroup.Children}
+          onChange={() => {
+            dispatch(setAgeGroup({ ageGroup: 'Children', value: !filters.ageGroup.Children }));
+          }}
+        >
+          Children (0 - 12 years)
+        </Checkbox>
+        <Checkbox
+          label="teenagers"
+          isSelected={filters.ageGroup.Teenagers}
+          onChange={() =>
+            dispatch(setAgeGroup({ ageGroup: 'Teenagers', value: !filters.ageGroup.Teenagers }))
+          }
+        >
+          Teenagers (12 - 18 years)
+        </Checkbox>
+        <Checkbox
+          label="adults"
+          isSelected={filters.ageGroup.Adults}
+          onChange={() =>
+            dispatch(setAgeGroup({ ageGroup: 'Adults', value: !filters.ageGroup.Adults }))
+          }
+        >
+          Adults (18 - 64 years)
+        </Checkbox>
+        <Checkbox
+          label="seniors"
+          isSelected={filters.ageGroup.Seniors}
+          onChange={() =>
+            dispatch(setAgeGroup({ ageGroup: 'Seniors', value: !filters.ageGroup.Seniors }))
+          }
+        >
+          Seniors (65+ years)
+        </Checkbox>
       </div>
 
       <div className="flex flex-col gap-1">
         <p className="font-semibold">Tour Rating</p>
-
-        {[4, 3, 2, 1].map((item) => (
-          <StarRatings
-            rating={item}
-            key={item}
-            starRatedColor="orange"
-            starDimension="20px"
-            starSpacing="3px"
-          />
-        ))}
+        <RadioGroup
+          value={filters.tourRating}
+          onChange={(event) => {
+            dispatch(setTourRating(event.target.value));
+          }}
+        >
+          {[4, 3, 2, 1].map((item) => (
+            <Radio key={item} value={item.toString()}>
+              <StarRatings
+                rating={item}
+                starRatedColor="orange"
+                numberOfStars={5}
+                name="rating"
+                starDimension="20px"
+                starSpacing="1px"
+              />
+              <span className="ml-2 text-xs">{`(${item} & above)`}</span>
+            </Radio>
+          ))}
+        </RadioGroup>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Filters;
