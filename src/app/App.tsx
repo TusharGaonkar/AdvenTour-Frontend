0f417490878b1d0ea326b5c920c578266f8d2e42
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:42:20 +0530

    re-authentication added to persist  user login state when the user exits and reopens the browser

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,24 +8,30 @@ import SignupForm from '../pages/SignUpForm';
 import AdventourLandingPage from '../pages/AdventourLandingPage';
 import ToursPage from '../pages/ToursPage';
 import TourDetailsPage from '../pages/TourDetailsPage';
+import { reAuthenticate } from '../redux/slices/userSlice';
 import '../index.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(reAuthenticate());
+  }, []); // when our app mounts , reauthenticate users if there is a token in the cookie!!
+
   return (
-    <Provider store={store}>
-      <NextUIProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<AdventourLandingPage />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<SignupForm />} />
-            <Route path="/tours" element={<ToursPage />} />
-            <Route path="/tours/:id" element={<TourDetailsPage />} />
-          </Routes>
-          <Toaster position="top-right" reverseOrder={false} />
-        </BrowserRouter>
-      </NextUIProvider>
-    </Provider>
+    <NextUIProvider>
+      <Routes>
+        <Route path="/" element={<AdventourLandingPage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<SignupForm />} />
+        <Route path="/tours" element={<ToursPage />} />
+        <Route path="/tours/:id" element={<TourDetailsPage />} />
+      </Routes>
+      <Toaster position="top-right" reverseOrder={false} />
+    </NextUIProvider>
   );
 }
 
