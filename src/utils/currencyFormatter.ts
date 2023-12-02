01e8c35d5d441d7287795f2d586cc51f32cd523e
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:59:05 +0530

    currency formatter for indian rupees as a utility

---
+++ src/utils/currencyFormatter.ts
@@ -0,0 +1,8 @@
+export default function formatToINR(amount: number): string {
+  const formatter = new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+    minimumFractionDigits: 0,
+  });
+  return formatter.format(amount);
+}
