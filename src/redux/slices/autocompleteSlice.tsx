94236c9fec790a5e23c0bdf3698fc2ea1c563a8c
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:53:31 +0530

    fix minor path error in the url

--- src/redux/slices/autocompleteSlice.tsx
+++ src/redux/slices/autocompleteSlice.tsx
@@ -4,7 +4,7 @@ const autocompleteSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getSuggestions: builder.query({
       query: (inputString: string) => ({
-        url: `tours/suggestions/?query=${inputString}`,
+        url: `tours/suggestions?query=${inputString}`,
         method: 'GET',
       }),
     }),
