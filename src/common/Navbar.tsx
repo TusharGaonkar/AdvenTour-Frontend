c6d9faa04c18f2700cdca3dc749d2b8983ef8067
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:43:45 +0530

    Navbar layout updated to handle view changes on change of user roles

---
+++ src/common/Navbar.tsx
@@ -0,0 +1,127 @@
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from '@nextui-org/react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useLocation } from 'react-router-dom';
+import { logoutUser } from '../redux/slices/userSlice';
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Avatar,
+  User,
+} from '@nextui-org/react';
+import SearchBar from '../features/Tours/SearchBar';
+
+const landingPageLinks = [
+  {
+    label: 'All tours',
+    link: '/tours',
+  },
+
+  {
+    label: 'Spotlight',
+    link: '#',
+  },
+];
+
+const UserDropDown = ({
+  userName,
+  userRole,
+  avatar,
+}: {
+  userName: string;
+  userRole: string;
+  avatar: string;
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <Dropdown placement="bottom-start">
+      <DropdownTrigger>
+        <User
+          as="button"
+          avatarProps={{
+            isBordered: true,
+            src: 'https://i.pravatar.cc/150?u=a042581f4e29026024d',
+          }}
+          className="transition-transform"
+          name={userName}
+        />
+      </DropdownTrigger>
+      <DropdownMenu aria-label="User Actions" variant="flat">
+        <DropdownItem key="user-profile" className="gap-2 h-14">
+          <p className="font-bold">Signed in as</p>
+          <p className="font-bold">{userName}</p>
+        </DropdownItem>
+        <DropdownItem key="settings">Profile Settings</DropdownItem>
+        <DropdownItem key="logout" color="danger" onClick={() => dispatch(logoutUser())}>
+          Logout
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+const NavBar = () => {
+  const { pathname } = useLocation();
+
+  const { user, isLoggedIn } = useSelector((state) => state.userInfo);
+
+  return (
+    <Navbar className="text-xs">
+      <NavbarBrand>
+        <p className="font-bold text-inherit">
+          <NavLink to="/">AdvenTour</NavLink>
+        </p>
+      </NavbarBrand>
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        {pathname === '/' &&
+          landingPageLinks.map((item) => (
+            <NavbarItem key={item.label}>
+              <NavLink to={item.link} className={pathname === item.link ? 'font-semibold' : ''}>
+                {item.label}
+              </NavLink>
+            </NavbarItem>
+          ))}
+      </NavbarContent>
+      <NavbarContent justify="end" className="">
+        {isLoggedIn ? (
+          <>
+            {pathname != '/' && (
+              <NavbarItem key="search" className="w-[400px]">
+                <SearchBar />
+              </NavbarItem>
+            )}
+            <NavbarItem key="contribute">
+              <NavLink to="#">Contribute</NavLink>
+            </NavbarItem>
+            <NavbarItem key="Bookmarked Tours">
+              <NavLink to="#">Bookmarks</NavLink>
+            </NavbarItem>
+            <NavbarItem key="bookings">
+              <NavLink to="#">My bookings</NavLink>
+            </NavbarItem>
+            <NavbarItem
+              key="profile"
+              className="flex items-center justify-start pr-2 ml-2 text-white rounded-full bg-slate-500"
+            >
+              <UserDropDown userName={user?.userName} userRole={user?.role} avatar={user?.avatar} />
+            </NavbarItem>
+          </>
+        ) : (
+          <>
+            <NavbarItem className="hidden lg:flex">
+              <NavLink to="/login">Login</NavLink>
+            </NavbarItem>
+            <NavbarItem>
+              <Button color="primary" variant="flat">
+                <NavLink to="/register">Register</NavLink>
+              </Button>
+            </NavbarItem>
+          </>
+        )}
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default NavBar;
