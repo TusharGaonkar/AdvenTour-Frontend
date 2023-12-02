5f630255d9e86ae6c2d32c771f4e75bf7d282539
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:44:59 +0530

    moved search bar to the navbar component

--- src/features/Tours/FindTours.tsx
+++ src/features/Tours/FindTours.tsx
@@ -1,16 +1,14 @@
 import { Button } from '@nextui-org/react';
 import NearByTours from './NearByTours';
-import SearchBar from './SearchBar';
 import SelectGroupSize from './SelectGroupSize';
 import SelectTourDate from './SelectTourDate';
 
 const FindTours = () => (
   <div className="flex items-center justify-between w-full gap-8 px-6 py-8">
-    <SearchBar />
-    <SelectGroupSize />
     <NearByTours />
+    <SelectGroupSize />
     <SelectTourDate />
-    <Button className="w-full h-full py-4 bg-teal-400"> Search Tours</Button>
+    <Button className="w-full h-full py-4 text-white bg-primary"> Search Tours</Button>
   </div>
 );
 
