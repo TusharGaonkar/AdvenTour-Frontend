f8fa16a2fbab05758fe6724c2337cbddfd2f3611
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-03 21:12:24 +0530

    props added to tour description

--- src/features/TourDetails/TourDescription.tsx
+++ src/features/TourDetails/TourDescription.tsx
@@ -1,15 +1,19 @@
-import { tour } from '../../pages/test';
-const TourDescription = () => {
-  return (
-    <div className="flex flex-col items-start gap-3">
-      <h1 className="font-semibold text-md"> About</h1>
-      <p>{tour.description} </p>
-      <p>
-        from <span className="text-2xl font-semibold">₹{tour.priceInRupees}</span> per adult (price
-        varies by group size)
-      </p>
-    </div>
-  );
-};
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-closing-tag-location */
+import formatToINR from '../../utils/currencyFormatter';
+
+const TourDescription = ({ tour }: { tour: Record<string, unknown> }) => (
+  <div className="flex flex-col items-start gap-3">
+    <h1 className="font-semibold text-md"> About</h1>
+    <p>{tour.description as string}</p>
+    <p>
+      from
+      <span className="text-2xl font-semibold">{`₹${formatToINR(
+        tour.price as number
+      )}/person`}</span>
+      per adult (price varies by group size)
+    </p>
+  </div>
+);
 
 export default TourDescription;
