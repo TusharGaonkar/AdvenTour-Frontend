59909fc61dcc9b610591624dddc8fef845ea777f
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-03 20:59:25 +0530

    added dynamic markers on the map , based on the response

--- src/features/TourDetails/Itinerary.tsx
+++ src/features/TourDetails/Itinerary.tsx
@@ -1,29 +1,60 @@
-import Map, { Marker } from 'react-map-gl';
+import Map, { Marker, Popup } from 'react-map-gl';
 import { useState } from 'react';
 import Timeline from './Timeline';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const Itinerary = () => {
+const Itinerary = ({ tour }: { tour: Record<string, unknown> }) => {
+  const { tourLocation } = tour;
+
   const [viewState, setViewState] = useState({
-    longitude: -100,
-    latitude: 40,
-    zoom: 10,
+    longitude: tourLocation.coordinates.at(1),
+    latitude: tourLocation.coordinates.at(0),
+    zoom: 14,
   });
   const token =
     'pk.eyJ1IjoidHVzaGFyZGciLCJhIjoiY2xwOGQxbmozMms3bzJrczRla2wzZDk1aiJ9.Gj3D87Qq2LxjfYlt-ARpxA';
   return (
     <>
       <h1 className="font-semibold">Detailed itinerary</h1>
-      <div className="grid w-full grid-cols-3 gap-6 p-4 mx-auto">
-        <Timeline />
+      <div className="grid w-full grid-cols-3 p-4 mx-auto space-x-6">
+        <Timeline tour={tour} setViewState={setViewState} />
         <div className="col-span-2">
           <Map
             mapboxAccessToken={token}
             {...viewState}
             onMove={(evt) => setViewState(evt.viewState)}
-            mapStyle="mapbox://styles/mapbox/outdoors-v12"
+            mapStyle="mapbox://styles/mapbox/streets-v12"
           >
-            <Marker longitude={-122.4} latitude={37.8} anchor="bottom" />
+            <Marker
+              longitude={tourLocation?.coordinates.at(1)}
+              latitude={tourLocation?.coordinates.at(0)}
+              anchor="bottom"
+            />
+
+            {tour?.itinerary
+              .map(
+                ({
+                  day,
+                  activities,
+                }: {
+                  day: Record<string, unknown>;
+                  activities: Record<string, unknown>[];
+                }) =>
+                  activities.map(({ location, place, activityName, _id }) => (
+                    <Popup
+                      longitude={location.coordinates.at(1)}
+                      latitude={location.coordinates.at(0)}
+                      anchor="bottom"
+                      closeButton={false}
+                      key={_id}
+                    >
+                      <p className="font-bold text-blue-600">{`Day ${day}`}</p>
+                      <p className="font-semibold">{place}</p>
+                      <p>{activityName}</p>
+                    </Popup>
+                  ))
+              )
+              .flat(1)}
           </Map>
         </div>
       </div>
