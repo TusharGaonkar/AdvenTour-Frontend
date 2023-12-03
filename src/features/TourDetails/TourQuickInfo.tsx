e85c1477a5fd31e7c2149413912184b0687725df
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-03 21:15:36 +0530

    props added to tour quick info component

--- src/features/TourDetails/TourQuickInfo.tsx
+++ src/features/TourDetails/TourQuickInfo.tsx
@@ -2,24 +2,23 @@ import { Chip } from '@nextui-org/react';
 import { IoIosPeople, IoMdTime } from 'react-icons/io';
 import { MdOutlineTimeline, MdOutlineTour } from 'react-icons/md';
 import { RiSpeakFill } from 'react-icons/ri';
-import { tour } from '../../pages/test';
 
-const TourQuickInfo = () => (
+const TourQuickInfo = ({ tour }: { tour: Record<string, unknown> }) => (
   <div className="flex flex-col items-start gap-3">
     <Chip variant="flat" color="default" startContent={<IoIosPeople className="text-2xl" />}>
-      {`Age group: ${tour.ageGroups.minAge} - ${tour.ageGroups.maxAge}`}
+      {`Age group: ${tour?.ageGroups?.minAge as number} - ${tour?.ageGroups?.maxAge as number}`}
     </Chip>
     <Chip color="default" variant="flat" startContent={<MdOutlineTimeline className="text-2xl" />}>
-      {`Tour duration : ${tour.duration} Days`}
+      {`Tour duration : ${tour?.tourDurationInDays} Days`}
     </Chip>
     <Chip variant="flat" color="default" startContent={<MdOutlineTour className="text-2xl" />}>
-      {`Tour difficulty: ${tour.tourDifficulty}`}
+      {`Tour difficulty: ${tour?.tourDifficulty}`}
     </Chip>
     <Chip color="default" variant="flat" startContent={<IoMdTime className="text-2xl" />}>
-      {`Tour start time : ${tour.tourStartTime}`}
+      {`Tour start time : ${tour?.tourStartTime}`}
     </Chip>
     <Chip color="default" variant="flat" startContent={<RiSpeakFill className="text-2xl" />}>
-      {`Tour guide languages : ${tour.liveGuideLanguages.join(', ')}`}
+      {`Tour guide languages : ${(tour?.liveGuideLanguages as string[]).join(', ')}`}
     </Chip>
   </div>
 );
