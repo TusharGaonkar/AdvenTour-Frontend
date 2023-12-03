538ff9fd20bd06889e8d70001784644c2a0d6bb9
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-03 21:19:47 +0530

    props + few components updated

--- src/pages/TourDetailsPage.tsx
+++ src/pages/TourDetailsPage.tsx
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/order
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Divider, Progress, Spinner } from '@nextui-org/react';
+import { useGetIndividualTourInfoQuery } from '../redux/slices/getIndividualTourInfoSlice';
 import NavBar from '../common/Navbar';
-import { Divider, Progress } from '@nextui-org/react';
 import 'react-photo-view/dist/react-photo-view.css';
 import TourTitle from '../features/TourDetails/TourTitle';
 import BookMarkTour from '../features/TourDetails/BookMarkTour';
@@ -12,36 +15,53 @@ import BookTour from '../features/TourDetails/BookTour';
 import Itinerary from '../features/TourDetails/Itinerary';
 import TourReviews from '../features/TourDetails/TourReviews';
 
-const TourDetailsPage = () => (
-  <>
-    {/* <Progress size="sm" isIndeterminate aria-label="Loading..." className="w-screen" /> */}
-    <div className="flex flex-col max-w-6xl mx-auto gap-7">
-      <NavBar />
-      <div className="flex items-start justify-between">
-        <TourTitle />
-        <BookMarkTour />
-      </div>
+const TourDetailsPage = () => {
+  const { id } = useParams();
+  const { data: tourData, status, error } = useGetIndividualTourInfoQuery(id);
 
-      <RenderTourImages />
-      <div className="grid items-start grid-cols-2">
-        <div className="flex flex-col gap-7">
-          <TourDescription />
-          <Divider />
-          <TourQuickInfo />
-          <Divider />
-          <TourDetailedInfo />
-        </div>
+  return (
+    <>
+      <Progress
+        size="sm"
+        isIndeterminate={status === 'pending'}
+        aria-label="Loading..."
+        className="w-screen"
+      />
+      <div className="flex flex-col max-w-6xl mx-auto gap-7">
+        <NavBar />
+        {status === 'fulfilled' && tourData.totalResults === 1 ? (
+          <>
+            <div className="flex items-start justify-between">
+              <TourTitle tour={tourData?.data?.tour} />
+              <BookMarkTour />
+            </div>
+            <RenderTourImages tour={tourData.data.tour} />
+            <div className="grid items-start grid-cols-2">
+              <div className="flex flex-col gap-7">
+                <TourDescription tour={tourData?.data?.tour} />
+                <Divider />
+                <TourQuickInfo tour={tourData?.data?.tour} />
+                <Divider />
+                <TourDetailedInfo tour={tourData?.data?.tour} />
+              </div>
 
-        <div className="sticky top-20">
-          <BookTour />
-        </div>
+              <div className="sticky top-20">
+                <BookTour />
+              </div>
+            </div>
+            <Divider />
+            <Itinerary tour={tourData?.data?.tour} />
+            <Divider />
+            <TourReviews />
+          </>
+        ) : (
+          <div className="flex items-center justify-center h-[60vh]">
+            <Spinner color="danger" size="lg" />
+          </div>
+        )}
       </div>
-      <Divider />
-      <Itinerary />
-      <Divider />
-      <TourReviews />
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default TourDetailsPage;
