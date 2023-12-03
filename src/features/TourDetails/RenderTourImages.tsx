3ea70d0d1544a08c2e791125e53dd42d8b96a4f4
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-03 21:00:03 +0530

    layout fixes in the tour images , fixed ui bugs

--- src/features/TourDetails/RenderTourImages.tsx
+++ src/features/TourDetails/RenderTourImages.tsx
@@ -1,38 +1,30 @@
 import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Image } from '@nextui-org/react';
-import React from 'react';
 
-const RenderTourImages = () => {
-  const images = [
-    'https://media2.thrillophilia.com/images/photos/000/050/054/original/1539406781_original_1072567361.jpg?gravity=center&width=1280&height=642&crop=fill&quality=auto&fetch_format=auto&flags=strip_profile&format=jpg&sign_url=true',
-    'https://qph.cf2.quoracdn.net/main-qimg-7c1241976e1c427f600b2c7c5c244a3b-lq',
-    'https://www.fabhotels.com/blog/wp-content/uploads/2018/02/shutterstock_780660901.jpg',
-  ];
-
-  return (
-    <PhotoProvider>
-      <div className="flex flex-row gap-3 h-[440px] cursor-pointer ">
-        <div className="overflow-hidden rounded-xl">
-          <PhotoView key={Math.random()} src={images[0]}>
-            <Image src={images[0]} alt="" className="w-full object-cover h-[440px]" />
-          </PhotoView>
-        </div>
-
-        <div className="flex flex-col gap-2 h-[440px]">
-          <div className="overflow-hidden rounded-xl">
-            <PhotoView key={Math.random()} src={images[1]}>
-              <Image src={images[1]} alt="" className="object-cover " />
-            </PhotoView>
-          </div>
-          <div className="overflow-hidden rounded-xl">
-            <PhotoView key={Math.random()} src={images[2]}>
-              <img src={images[2]} alt="" className="object-cover" />
-            </PhotoView>
-          </div>
-        </div>
+const RenderTourImages = ({ tour }: { tour: Record<string, unk> }) => (
+  <PhotoProvider>
+    <div className="grid grid-cols-2 grid-rows-2  h-[440px] cursor-pointer gap-3 justify-center cursor-pointer">
+      <div className="w-full h-full row-span-2 overflow-hidden rounded-xl">
+        <PhotoView key={tour?.mainCoverImage} src={tour?.mainCoverImage}>
+          <img src={tour?.mainCoverImage} alt="" className="object-cover w-full h-full" />
+        </PhotoView>
       </div>
-    </PhotoProvider>
-  );
-};
+
+      {tour?.additionalImages?.map((image: string, index: number) =>
+        index < 2 ? (
+          <div className="w-full h-full overflow-hidden rounded-xl">
+            <PhotoView key={image} src={image}>
+              <Image
+                src={image}
+                alt="additional-images-tour"
+                className="object-cover w-full h-full"
+              />
+            </PhotoView>
+          </div>
+        ) : null
+      )}
+    </div>
+  </PhotoProvider>
+);
 
 export default RenderTourImages;
