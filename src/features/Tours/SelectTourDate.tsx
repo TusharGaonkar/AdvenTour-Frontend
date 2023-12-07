5609b58f7d8a7ae9f22cf4c28cc75d264981eb65
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-07 14:37:16 +0530

    feat: Filter by Tour date  component

--- src/features/Tours/SelectTourDate.tsx
+++ src/features/Tours/SelectTourDate.tsx
@@ -1,6 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import { Input } from '@nextui-org/react';
-const SelectTourDate = () => {
-  return <Input type="date" label="Tour Date" placeholder="Select a tour date" />;
+
+const SelectTourDate = ({
+  tourDate,
+  setTourDate,
+}: {
+  tourDate: string;
+  setTourDate: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const [date, setDate] = useState(tourDate);
+  const nextDate = new Date();
+  nextDate.setDate(nextDate.getDate() + 1);
+
+  useEffect(() => {
+    setTourDate(date);
+  }, [date, setTourDate]);
+
+  return (
+    <Input
+      value={date}
+      type="date"
+      label="Tour Date"
+      placeholder="Select a tour date"
+      onChange={(event) => setDate(event.target.value)}
+      min={nextDate.toISOString().split('T')[0]}
+    />
+  );
 };
 
 export default SelectTourDate;
