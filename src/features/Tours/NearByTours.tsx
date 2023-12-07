76b16d90f042044f2f1aede44c01d278a3af7cac
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-07 14:35:26 +0530

    feat: Get location {long , lat}  of the user using inbuilt navigator

--- src/features/Tours/NearByTours.tsx
+++ src/features/Tours/NearByTours.tsx
@@ -1,8 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Switch } from '@nextui-org/react';
+import toast from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+import { isMobile } from 'react-device-detect';
 
-const NearByTours = () => {
-  const [isSelected, setIsSelected] = useState(false);
+const NearByTours = ({
+  setCoords,
+}: {
+  setCoords: React.Dispatch<React.SetStateAction<number[] | null>>;
+}) => {
+  const { getNearbyTours } = useSelector((state) => state.filterToursQueryString);
+  const [isSelected, setIsSelected] = useState<boolean>(!!getNearbyTours);
+  useEffect(() => {
+    if (isSelected) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            if (!isMobile) {
+              toast.success(
+                'Please note that the detected location can be inaccurate due to limited access to GPS on this device!',
+                { className: 'font-medium text-xs rounded-xl', duration: 4000 }
+              );
+            }
+            setCoords([position.coords.longitude, position.coords.latitude]);
+          },
+          (error) => {
+            setIsSelected(false);
+            setCoords(null);
+            toast.error(`${error.message}, please reset the permission`, {
+              className: 'text-xs font-semibold ',
+            });
+          },
+          {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+          }
+        );
+      }
+    } else {
+      setCoords(null);
+    }
+  }, [isSelected, setCoords]);
+
   return (
     <div className="flex items-center self-stretch justify-between w-full p-3 bg-gray-100 rounded-xl">
       <p className="text-xs font-semibold"> Get nearby tours</p>
