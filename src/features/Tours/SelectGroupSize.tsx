e503007be55fcbea875c26fc2f81e16f66e81ae1
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-07 14:36:33 +0530

    feat: Filter by group size implemented

--- src/features/Tours/SelectGroupSize.tsx
+++ src/features/Tours/SelectGroupSize.tsx
@@ -1,15 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import { Select, SelectItem } from '@nextui-org/react';
 
-const SelectGroupSize = () => {
-  const groupSize = ['1', '2', '3', '4'];
+const SelectGroupSize = ({
+  groupSize,
+  setGroupSize,
+}: {
+  groupSize: number;
+  setGroupSize: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const allGroupSizes = ['1', '2', '3', '4'];
+  const [input, setInput] = useState(groupSize);
+
+  useEffect(() => {
+    setGroupSize(+input);
+  }, [input, setGroupSize]);
+
   return (
     <Select
       label="Group Size"
       placeholder="Select group size"
       className="max-w-xs"
-      defaultSelectedKeys={['1']}
+      defaultSelectedKeys={[input.toString()]}
+      onChange={(event) => {
+        if (event.target.value) setInput(+event.target.value);
+      }}
     >
-      {groupSize.map((item) => (
+      {allGroupSizes.map((item) => (
         <SelectItem value={item} key={item}>
           {item}
         </SelectItem>
