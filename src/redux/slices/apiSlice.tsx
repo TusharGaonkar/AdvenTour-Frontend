facf997635616ed71ed838b14416636e5773b12a
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-07 14:42:13 +0530

    feat: Tagtypes added in the slice to cache the json response

--- src/redux/slices/apiSlice.tsx
+++ src/redux/slices/apiSlice.tsx
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const BASE_URL = 'http://localhost:2000/api/v-1.0';
 
 const apiSlice = createApi({
+  tagTypes: ['Bookmarks', 'Tours'],
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' }), // include credentials as true , else cookies will not be set if you are requesting from adventour api
   endpoints: () => ({}),
