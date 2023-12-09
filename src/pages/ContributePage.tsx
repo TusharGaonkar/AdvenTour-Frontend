8b2d67f84857eb572061cbea5decce9d85881288
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-10 02:02:22 +0530

    feat: New contribute tour page initial layout

--- src/pages/ContributePage.tsx
+++ src/pages/ContributePage.tsx
@@ -1,10 +1,45 @@
 import NavBar from '../common/Navbar';
+import { Button, Input } from '@nextui-org/react';
+import useCustomMultiForm from '../hooks/useCustomMultiForm';
+import TourBriefForm from '../features/ContributeTour/TourBriefForm';
+import TourImagesForm from '../features/ContributeTour/TourImagesForm';
+
+const ContributeMultiStepForm = () => {
+  const items = [
+    {
+      label: 'Tour Brief',
+      content: <TourBriefForm />,
+    },
+    {
+      label: 'Add Tour Images',
+      content: <TourImagesForm />,
+    },
+  ];
+  const { activeIndex, activeItem, isStart, isEnd, handleNext, handlePrev } =
+    useCustomMultiForm(items);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-w-full gap-6">
+      <h1 className="text-xl font-bold">{activeItem.label}</h1>
+      <form className="flex flex-col w-full gap-5">{activeItem.content}</form>
+      <div className="flex justify-end min-w-full gap-3">
+        <Button onClick={handlePrev} disabled={isStart}>
+          Previous
+        </Button>
+        <Button onClick={handleNext} disabled={isEnd}>
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 const ContributePage = () => {
   return (
     <div className="flex flex-col w-full max-w-5xl gap-6 mx-auto">
       <NavBar />
-      <h1 className="text-3xl font-bold">Add a new tour</h1>
+      <h1 className="text-xl font-bold">Add a new tour</h1>
+      <ContributeMultiStepForm />
     </div>
   );
 };
