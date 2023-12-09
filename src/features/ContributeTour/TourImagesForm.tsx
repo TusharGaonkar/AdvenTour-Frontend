f16d68a403c49f08d0ab4afaed8290a03afb09ce
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-10 02:02:59 +0530

    feat: Initial multi form items layout

---
+++ src/features/ContributeTour/TourImagesForm.tsx
@@ -0,0 +1,14 @@
+import { Input } from '@nextui-org/react';
+
+const TourImagesForm = () => {
+  return (
+    <Input
+      type="file"
+      label="Tour Main Cover Image"
+      labelPlacement="outside"
+      placeholder="Add Tour Main Cover Image"
+    />
+  );
+};
+
+export default TourImagesForm;
