10f9397816bc1109bf3ea0c051ac9a2091300978
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-11 18:37:54 +0530

    feat:  Chip added to move to a given index directly and changes , and minor bug fixes

--- src/pages/ContributePage.tsx
+++ src/pages/ContributePage.tsx
@@ -1,26 +1,49 @@
+import { Button, Chip, Divider } from '@nextui-org/react';
 import NavBar from '../common/Navbar';
-import { Button, Input } from '@nextui-org/react';
 import useCustomMultiForm from '../hooks/useCustomMultiForm';
-import TourBriefForm from '../features/ContributeTour/TourBriefForm';
-import TourImagesForm from '../features/ContributeTour/TourImagesForm';
+import TourBasicInfoForm from '../features/ContributeTour/TourBasicInfoForm';
+import TourPriceLocationForm from '../features/ContributeTour/TourPriceLocationForm';
+import TourAdditionalInfoForm from '../features/ContributeTour/TourAdditionalInfoForm';
+import DetailedItineraryForm from '../features/ContributeTour/TourDetailedItineraryForm';
 
 const ContributeMultiStepForm = () => {
   const items = [
     {
-      label: 'Tour Brief',
-      content: <TourBriefForm />,
+      label: 'Tour Basic Info',
+      content: <TourBasicInfoForm />,
     },
     {
-      label: 'Add Tour Images',
-      content: <TourImagesForm />,
+      label: 'Tour Price & Location',
+      content: <TourPriceLocationForm />,
     },
+    {
+      label: 'Tour Additional Info',
+      content: <TourAdditionalInfoForm />,
+    },
+    { label: 'Tour Detailed Itinerary', content: <DetailedItineraryForm /> },
   ];
-  const { activeIndex, activeItem, isStart, isEnd, handleNext, handlePrev } =
+
+  const { activeIndex, activeItem, isStart, isEnd, handleNext, handlePrev, jumpToIndex } =
     useCustomMultiForm(items);
 
   return (
-    <div className="flex flex-col items-center justify-center min-w-full gap-6">
-      <h1 className="text-xl font-bold">{activeItem.label}</h1>
+    <div className="flex flex-col items-center justify-center w-full min-w-full gap-6 mx-auto">
+      <div className="flex items-center w-full justify-evenly">
+        {items.map(({ label }, index) => (
+          <div className="flex items-center" key={label}>
+            <Chip
+              variant="shadow"
+              color={activeIndex === index ? 'danger' : 'default'}
+              className="cursor-pointer"
+              onClick={() => jumpToIndex(index)}
+              size="lg"
+            >
+              {label}
+            </Chip>
+            {index !== items.length - 1 && <Divider key={label} />}
+          </div>
+        ))}
+      </div>
       <form className="flex flex-col w-full gap-5">{activeItem.content}</form>
       <div className="flex justify-end min-w-full gap-3">
         <Button onClick={handlePrev} disabled={isStart}>
