d6ca3557cc73e5e087e0ea2b4c05335bd15b7d16
Tushar <gaonkar.tushar01@gmail.com>
2023-12-18 22:14:04 +0530

    feat: new FAQSlice reducer

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import apiSlice from '../redux/slices/apiSlice';
 import userSlice from '../redux/slices/userSlice';
 import filterToursSlice from '../redux/slices/filterToursSlice';
+import FAQSlice from '../redux/slices/FAQSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     [userSlice.name]: userSlice.reducer,
     [filterToursSlice.name]: filterToursSlice.reducer,
+    [FAQSlice.name]: FAQSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
