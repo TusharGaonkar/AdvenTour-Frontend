805039bb90c4685f80d7e0b579e85695027bfafc
Tushar <gaonkar.tushar01@gmail.com>
2023-12-18 22:18:51 +0530

    fix: Changes in the items prop & code refactor

--- src/pages/ContributePage.tsx
+++ src/pages/ContributePage.tsx
@@ -1,26 +1,57 @@
 import { Button, Chip, Divider } from '@nextui-org/react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import NavBar from '../common/Navbar';
 import useCustomMultiForm from '../hooks/useCustomMultiForm';
 import TourBasicInfoForm from '../features/ContributeTour/TourBasicInfoForm';
 import TourPriceLocationForm from '../features/ContributeTour/TourPriceLocationForm';
 import TourAdditionalInfoForm from '../features/ContributeTour/TourAdditionalInfoForm';
 import DetailedItineraryForm from '../features/ContributeTour/TourDetailedItineraryForm';
+import {
+  contributeTourFormSchema,
+  type ContributeTourFormSchemaType,
+} from '../validators/ContributeTourFormValidator';
+import { useEffect, useState } from 'react';
 
 const ContributeMultiStepForm = () => {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+  } = useForm<ContributeTourFormSchemaType>({
+    resolver: zodResolver(contributeTourFormSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      address: '',
+      priceInRupees: 2000,
+      discountInRupees: 0,
+      maxPeoplePerBooking: 2,
+      tourDurationInDays: 1,
+      tourMaxCapacity: 30,
+      whatsIncluded: 'Accommodation, Food, Transportation, Activities, Guide',
+      whatsNotIncluded: 'Travel Insurance, Tour Guide Certification, Personal Expenses',
+    },
+    mode: 'onChange',
+  });
+
   const items = [
     {
       label: 'Tour Basic Info',
-      content: <TourBasicInfoForm />,
+      content: TourBasicInfoForm,
     },
     {
       label: 'Tour Price & Location',
-      content: <TourPriceLocationForm />,
+      content: TourPriceLocationForm,
     },
     {
       label: 'Tour Additional Info',
-      content: <TourAdditionalInfoForm />,
+      content: TourAdditionalInfoForm,
     },
-    { label: 'Tour Detailed Itinerary', content: <DetailedItineraryForm /> },
+    {
+      label: 'Tour Detailed Itinerary',
+      content: DetailedItineraryForm,
+    },
   ];
 
   const { activeIndex, activeItem, isStart, isEnd, handleNext, handlePrev, jumpToIndex } =
@@ -44,7 +75,9 @@ const ContributeMultiStepForm = () => {
           </div>
         ))}
       </div>
-      <form className="flex flex-col w-full gap-5">{activeItem.content}</form>
+      <form className="flex flex-col w-full gap-5">
+        <activeItem.content register={register} errors={errors} getValues={getValues} />
+      </form>
       <div className="flex justify-end min-w-full gap-3">
         <Button onClick={handlePrev} disabled={isStart}>
           Previous
