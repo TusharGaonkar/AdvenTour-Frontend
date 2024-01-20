6a8add817acd54780ad0986d2e2b18e8ce4e0347
Tushar <gaonkar.tushar01@gmail.com>
2024-01-21 02:54:12 +0530

    feat✨: added validators for all the required fields

--- src/validators/ContributeTourFormValidator.ts
+++ src/validators/ContributeTourFormValidator.ts
@@ -1,15 +1,58 @@
 import { z } from 'zod';
 
+const MAX_FILE_SIZE = 3000000; // 3MB
+const ACCEPTED_IMAGE_TYPES = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
+
 const contributeTourFormSchema = z
   .object({
     title: z.string().min(15).max(30),
     description: z.string().min(15).max(100),
-    mainCoverImage: z.string().min(1), // base64 image string
-    additionalCoverImages: z.array(z.string().min(1)).min(1).max(10), // base64 image string
+    mainCoverImage: z
+      .any()
+      .refine((file) => {
+        if (file.length > 0) {
+          return file[0]?.size <= MAX_FILE_SIZE;
+        }
+        return true;
+      }, 'Max image size is 3MB , please upload less than 3MB.')
+      .refine((file) => {
+        if (file.length > 0) {
+          return ACCEPTED_IMAGE_TYPES.includes(file[0]?.type);
+        }
+        return true;
+      }, 'Only .jpg, .jpeg, .png and .webp formats are supported for main cover image.'),
+    additionalCoverImages: z
+      .array(z.any())
+      .length(3)
+      .refine((fileList) => {
+        if (fileList.length > 0) {
+          return fileList.every((file) => ACCEPTED_IMAGE_TYPES.includes(file.type));
+        }
+        return false;
+      }, 'Only .jpg, .jpeg, .png and .webp formats are supported for additional images.')
+      .refine((fileList) => {
+        if (fileList.length > 0) {
+          return fileList.every((file) => file.size <= MAX_FILE_SIZE);
+        }
+        return false;
+      }, 'Max additional image size is 3MB , please upload less than 3MB.'),
     address: z.string().min(6).max(300),
     tourLocation: z.object({
-      type: z.literal('Point'),
-      coordinates: z.array(z.number()).length(2),
+      type: z.literal('Point').optional(),
+      coordinates: z
+        .array(z.number())
+        .length(2)
+        .refine((coordinates) => {
+          if (coordinates.length === 2) {
+            return (
+              coordinates[0] >= -90 &&
+              coordinates[0] <= 90 &&
+              coordinates[1] >= -180 &&
+              coordinates[1] <= 180
+            );
+          }
+          return false;
+        }, 'Invalid coordinates in tour location. Latitude and longitude must be between -90 and 90 and -180 and 180 respectively.'),
     }),
     priceInRupees: z.number().min(1),
     discountInRupees: z.number().min(0),
@@ -17,17 +60,17 @@ const contributeTourFormSchema = z
     tourStartTime: z.string().min(1),
     tourMaxCapacity: z.number().min(1),
     maxPeoplePerBooking: z.number().min(1),
-    tourDifficulty: z.string().min(1),
-    // ageGroups: z
-    //   .object({
-    //     minAge: z.number().min(1),
-    //     maxAge: z.number().min(1),
-    //   })
-    //   .refine(({ minAge, maxAge }) => minAge <= maxAge, {
-    //     message: 'Minimum age cannot be greater than maximum age',
-    //   }),
+    tourDifficulty: z.enum(['Easy', 'Medium', 'Hard']),
+    ageGroups: z
+      .object({
+        minAge: z.number().min(1),
+        maxAge: z.number().min(1),
+      })
+      .refine(({ minAge, maxAge }) => minAge <= maxAge, {
+        message: 'Minimum age cannot be greater than maximum age',
+      }),
     liveGuideLanguages: z.array(z.string()).min(1),
-    tourStartDates: z.array(z.date()).min(1),
+    tourStartDates: z.array(z.string()).min(1),
     whatsIncluded: z
       .string()
       .trim()
@@ -60,15 +103,41 @@ const contributeTourFormSchema = z
             activityName: z.string().min(10).max(50),
             place: z.string().min(2).max(50),
             location: z.object({
-              type: z.literal('Point'),
-              coordinates: z.array(z.number()).length(2),
+              type: z.literal('Point').optional(),
+              coordinates: z
+                .array(z.number())
+                .length(2)
+                .refine((coordinates) => {
+                  if (coordinates.length === 2) {
+                    return (
+                      coordinates[0] >= -90 &&
+                      coordinates[0] <= 90 &&
+                      coordinates[1] >= -180 &&
+                      coordinates[1] <= 180
+                    );
+                  }
+                  return false;
+                }, 'Invalid coordinates in activities. Latitude and longitude must be between -90 and 90 and -180 and 180 respectively.'),
             }),
 
-            image: z.string().min(1),
+            image: z
+              .any()
+              .refine((file) => {
+                if (file.length > 0) {
+                  return file[0]?.size <= MAX_FILE_SIZE;
+                }
+                return true;
+              }, 'Max image size is 3MB in activity, please upload less than 3MB.')
+              .refine((file) => {
+                if (file.length > 0) {
+                  return ACCEPTED_IMAGE_TYPES.includes(file[0]?.type);
+                }
+                return true;
+              }, 'Only .jpg, .jpeg, .png and .webp formats are supported.'),
           })
         ),
-        foodIncluded: z.string().min(1),
-        accommodationIncluded: z.string().min(1),
+        foodIncluded: z.string().min(2).max(40),
+        accommodationIncluded: z.string().min(2).max(40),
       })
     ),
   })
