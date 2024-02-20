d122e98394f1e25cd3a497298f10c26d23e680ef
Tushar <gaonkar.tushar01@gmail.com>
2024-02-20 14:19:46 +0530

    feat: Display the payment details reads payment id from the url

---
+++ src/features/AdventourAdminPage/Payments.tsx
@@ -0,0 +1,92 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { IoArrowBackCircleOutline } from 'react-icons/io5';
+import { Button, Chip, Divider, Progress, Spinner } from '@nextui-org/react';
+import { useGetPaymentInfoQuery } from '../../redux/slices/admin-getAllBookingsSlice';
+import formatToINR from '../../utils/currencyFormatter';
+import toast from 'react-hot-toast';
+import { useEffect } from 'react';
+
+const Payments = () => {
+  const { paymentID } = useParams();
+  const {
+    data: paymentInfo,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPaymentInfoQuery(paymentID || '');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      let errorMessage = 'Invalid payment ID or internal server error';
+      if (error?.status === 401) {
+        errorMessage = 'Unauthorized access';
+      }
+      toast.error(errorMessage, {
+        className: 'font-medium text-xs',
+      });
+    }
+  }, [isError]);
+  return (
+    <>
+      <Progress
+        isIndeterminate={isLoading}
+        className="w-full overflow-hidden"
+        aria-label="progress"
+        size="sm"
+        color="danger"
+      />
+      <div className="mx-auto mt-[46px] shadow-md p-7 rounded-xl md:min-w-[550px] max-w-6xl">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold tracking-wide mb-8">
+            Payment Info for{' '}
+            <span>
+              <Chip color="success" variant="flat">
+                {paymentID}
+              </Chip>
+            </span>
+          </h1>
+
+          <Button
+            radius="full"
+            className="sm:text-medium text-sm min-w-min bg-primary text-white"
+            onClick={() => navigate('/admin/bookings')}
+          >
+            <span>
+              <IoArrowBackCircleOutline className="text-2xl" />
+            </span>
+            All Bookings
+          </Button>
+        </div>
+        <Divider />
+
+        <div className="grid mt-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          {isLoading && (
+            <Spinner color="danger" size="lg" className="mx-auto col-span-3" aria-label="loading" />
+          )}
+
+          {isSuccess &&
+            Object.entries(paymentInfo?.data?.payment || {})
+              .filter(([key, value]: [string, any]) => value && typeof value !== 'object')
+              .map(([key, value]: [string, any]) => (
+                <div
+                  key={key}
+                  className="flex flex-col gap-2 bg-blue-200/50 p-4 rounded-md min-w-[200px]"
+                >
+                  <p className="font-semibold text-sm uppercase">{key}</p>
+                  <p className="tracking-wide leading-relaxed text-sm">
+                    {['amount', 'tax', 'fee', 'amount_refunded'].includes(key)
+                      ? formatToINR(Number(value) / 100)
+                      : value}
+                  </p>
+                </div>
+              ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Payments;
