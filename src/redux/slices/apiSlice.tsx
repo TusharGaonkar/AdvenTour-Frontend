396a9e53255e37f19b1ff7ed0b10cd0df4822952
Tushar <gaonkar.tushar01@gmail.com>
2024-02-20 15:17:32 +0530

    feat: add interceptor in base query to inspect 401 auth errors , and redirect incase unhandled by components

--- src/redux/slices/apiSlice.tsx
+++ src/redux/slices/apiSlice.tsx
@@ -1,11 +1,31 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query';
 
 const BASE_URL = 'http://localhost:2000/api/v-1.0';
 
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' }); // include credentials to allow cookies to be sent with requests
+
+// intercept auth errors which are incase unhandled by protected route pages and redirect to login
+const interceptor: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+  args,
+  api,
+  extraOptions
+) => {
+  const result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    if (args.url.includes('admin')) {
+      window.location.href = '/admin/login';
+    } else {
+      window.location.href = '/login';
+    }
+  }
+  return result;
+};
+
 const apiSlice = createApi({
-  tagTypes: ['Bookmarks', 'Tours'],
+  tagTypes: ['Bookmarks', 'Tours', 'adminBookingsInfo'],
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL, credentials: 'include' }), // include credentials as true , else cookies will not be set if you are requesting from adventour api
+  baseQuery: interceptor,
   endpoints: () => ({}),
 });
 
