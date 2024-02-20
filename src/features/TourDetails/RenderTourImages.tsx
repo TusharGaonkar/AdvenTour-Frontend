6a43a4a1be2b567c198555ccd116e6f1615a841b
Tushar <gaonkar.tushar01@gmail.com>
2024-02-20 14:34:56 +0530

    style: changes in tour images preview

--- src/features/TourDetails/RenderTourImages.tsx
+++ src/features/TourDetails/RenderTourImages.tsx
@@ -3,14 +3,14 @@ import { Image } from '@nextui-org/react';
 
 const RenderTourImages = ({ tour }: { tour: Record<string, unknown> }) => (
   <PhotoProvider>
-    <div className="grid grid-cols-2 grid-rows-2  h-[440px] cursor-pointer gap-3 justify-center cursor-pointer">
+    <div className="grid grid-cols-2 grid-rows-2  h-[440px]  gap-3 justify-center cursor-pointer">
       <div className="w-full h-full row-span-2 overflow-hidden rounded-xl">
         <PhotoView key={tour?.mainCoverImage} src={tour?.mainCoverImage as string}>
           <img src={tour?.mainCoverImage as string} alt="" className="object-cover w-full h-full" />
         </PhotoView>
       </div>
 
-      {tour?.additionalImages?.map((image: string, index: number) =>
+      {tour?.additionalCoverImages?.map((image: string, index: number) =>
         index < 2 ? (
           <div className="w-full h-full overflow-hidden rounded-xl">
             <PhotoView key={image} src={image}>
