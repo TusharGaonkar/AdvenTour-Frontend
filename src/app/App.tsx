95b804afe5404ad2348d9b3cd39b1680d59b9014
Tushar <gaonkar.tushar01@gmail.com>
2024-02-20 14:11:54 +0530

    fix: Admin pages added with route protection

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,8 @@
 import { NextUIProvider } from '@nextui-org/react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { useLocation } from 'react-router-dom';
-import store from './store';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import LoginForm from '../pages/loginForm';
 import SignupForm from '../pages/SignUpForm';
 import AdventourLandingPage from '../pages/AdventourLandingPage';
@@ -11,10 +10,17 @@ import ToursPage from '../pages/ToursPage';
 import TourDetailsPage from '../pages/TourDetailsPage';
 import BookmarksPage from '../pages/BookmarksPage';
 import ContributePage from '../pages/ContributePage';
+import AdminLayout from '../pages/AdventourAdminPage';
+import AdminLoginForm from '../pages/AdminLoginForm';
+import Dashboard from '../features/AdventourAdminPage/Dashboard';
+import Users from '../features/AdventourAdminPage/Users';
+import AdminTourPreview from '../features/AdventourAdminPage/AdminTourPreview';
 import { reAuthenticate } from '../redux/slices/userSlice';
 import '../index.css';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import Payments from '../features/AdventourAdminPage/Payments';
+import DisplayBookings from '../features/AdventourAdminPage/DisplayBookings';
+import ProtectedAdminRoute from '../pages/ProtectedAdminRoute';
+import PageNotFound from '../pages/PageNotFound';
 
 function App() {
   const dispatch = useDispatch();
@@ -33,6 +39,17 @@ function App() {
         <Route path="/tours/:id" element={<TourDetailsPage />} />
         <Route path="/bookmarks" element={<BookmarksPage />} />
         <Route path="/contribute" element={<ContributePage />} />
+        <Route path="/admin/login" element={<AdminLoginForm />} />
+        <Route element={<ProtectedAdminRoute />}>
+          <Route element={<AdminLayout />}>
+            <Route index path="/admin" element={<Dashboard />} />
+            <Route path="/admin/users" element={<Users />} />
+            <Route path="/admin/bookings" element={<DisplayBookings />} />
+            <Route path="/admin/tours/:id" element={<AdminTourPreview />} />
+          </Route>
+        </Route>
+        <Route path="/admin/bookings/paymentInfo/:paymentID" element={<Payments />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Toaster position="top-right" reverseOrder={false} />
     </NextUIProvider>
