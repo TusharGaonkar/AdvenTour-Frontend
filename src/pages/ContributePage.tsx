55213015dadb1e1d9e74dc28144fd9aaa62763d7
Tushar <gaonkar.tushar01@gmail.com>
2024-02-20 14:42:21 +0530

    style: Navbar extends to whole screen while below components 'max-w-5xl'

--- src/pages/ContributePage.tsx
+++ src/pages/ContributePage.tsx
@@ -2,11 +2,13 @@ import ContributeMultiStepForm from '../features/ContributeTour/ContributeMultiS
 import NavBar from '../common/Navbar';
 
 const ContributePage = () => (
-  <div className="flex flex-col w-full max-w-5xl gap-6 mx-auto">
+  <>
     <NavBar />
-    <h1 className="text-xl font-bold">Add a new tour</h1>
-    <ContributeMultiStepForm />
-  </div>
+    <div className="flex flex-col w-full max-w-5xl gap-6 mx-auto">
+      <h1 className="text-xl font-bold">Add a new tour</h1>
+      <ContributeMultiStepForm />
+    </div>
+  </>
 );
 
 export default ContributePage;
