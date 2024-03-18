e22ee1bf46d8e0cd98bf0fb8708c1c82e055d558
Tushar <gaonkar.tushar01@gmail.com>
2024-03-18 18:16:51 +0530

    style: changes in the tour card layout and endpoints integrated

--- src/features/TourDetails/TourReviewCard.tsx
+++ src/features/TourDetails/TourReviewCard.tsx
@@ -1,43 +1,128 @@
-import { Card, CardHeader, CardFooter, Avatar, Divider, Image } from '@nextui-org/react';
+import {
+  Avatar,
+  Card,
+  CardFooter,
+  CardHeader,
+  Chip,
+  Divider,
+  Image,
+  Skeleton,
+} from '@nextui-org/react';
+import { format } from 'date-fns';
+import { useState } from 'react';
 import StarRating from 'react-star-ratings';
 
-const TourReviewCard: React.FC<{ review: any }> = ({ review }) => {
-  const { userAvatar, userName, ratings, reviewTitle, reviewText, date, image } = review;
+type TourReviewCardProps = {
+  title: string;
+  userName: string;
+  avatar: string;
+  description: string;
+  rating: number;
+  createdAt: Date;
+  travelGroup: string;
+  isLoading: boolean;
+  reviewImages?: string[];
+};
+
+const TourReviewCard = ({
+  title,
+  userName,
+  avatar,
+  description,
+  rating,
+  createdAt,
+  travelGroup,
+  reviewImages = [],
+  isLoading,
+}: TourReviewCardProps) => {
+  if (!title || !userName || !description || !rating || !createdAt) {
+    return null;
+  }
+  const descriptionLengthLimit = 300;
+  const [descriptionSliced, setDescriptionSliced] = useState(
+    description.slice(0, descriptionLengthLimit)
+  );
+  const maxCharExceeded = description.length > descriptionLengthLimit;
+  const [isReadMore, setIsReadMore] = useState<boolean>(maxCharExceeded);
+
+  const handleTextSlice = () => {
+    if (isReadMore) {
+      setDescriptionSliced(description); // reset to original length
+    } else {
+      setDescriptionSliced(description.slice(0, descriptionLengthLimit));
+    }
+    setIsReadMore(!isReadMore);
+  };
+
   return (
-    <Card className="flex flex-col gap-2 p-4">
-      <CardHeader className="flex flex-col items-start gap-2">
-        <div className="flex flex-row items-center gap-2">
-          <Avatar
-            isBordered
-            color="success"
-            src="https://i.pravatar.cc/150?u=a04258114e29026302d"
+    <Skeleton className="rounded-2xl border-1 shadow-lg" isLoaded={!isLoading}>
+      <Card className="flex flex-col gap-2 p-4">
+        <CardHeader className="flex flex-col items-start gap-2">
+          <div className="flex flex-row items-center justify-start gap-2">
+            <Avatar src={avatar} size="lg" showFallback isBordered name={userName} />
+            <p className="mx-1">{userName}</p>
+          </div>
+          <StarRating
+            rating={rating}
+            starRatedColor="#008000"
+            starDimension="20px"
+            starSpacing="01px"
           />
-          <p>{userName}</p>
-        </div>
-        <StarRating
-          key={ratings}
-          rating={ratings}
-          starRatedColor="green"
-          starDimension="18px"
-          starSpacing="3px"
-        />
-        <h1 className="text-sm font-semibold tracking-tight">{reviewTitle}</h1>
-        <Divider />
 
-        <p className="text-sm leading-tight tracking-tight ">{reviewText}</p>
-        <Image
-          src={image}
-          alt={reviewTitle}
-          className="object-cover p-1 rounded-sm"
-          width={100}
-          height={100}
-        />
-      </CardHeader>
+          <div className="flex gap-1 items-center">
+            <Chip
+              size="sm"
+              variant="flat"
+              className="bg-gradient-to-r from-neutral-300 to-stone-400"
+            >
+              Rated
+              <span className="mx-1 font-semibold text-sm">{`${rating}/5`}</span>
+            </Chip>
+            <Chip
+              size="sm"
+              variant="flat"
+              className="bg-gradient-to-r from-neutral-300 to-stone-400"
+            >
+              {travelGroup}
+            </Chip>
+          </div>
+          <h1 className="text-sm font-semibold tracking-tight mt-1">{title}</h1>
+          <Divider />
 
-      <CardFooter>
-        <p className="text-xs text-slate-500">{`Written on ${date}`}</p>
-      </CardFooter>
-    </Card>
+          <p className="text-sm leading-tight tracking-tight w-full break-words">
+            {descriptionSliced}
+          </p>
+          {description.length > 500 && (
+            <button
+              className="font-semibold text-xs underline"
+              onClick={() => {
+                handleTextSlice();
+              }}
+            >
+              {isReadMore ? 'Read More' : 'Read Less'}
+            </button>
+          )}
+          {reviewImages && reviewImages.length > 0 && (
+            <div className="flex gap-1 items-center">
+              {reviewImages?.map((image: string) => (
+                <Image
+                  key={image}
+                  src={image}
+                  alt={title}
+                  className="object-cover rounded-xl w-[105px] p-1"
+                />
+              ))}
+            </div>
+          )}
+        </CardHeader>
+
+        <CardFooter>
+          <p className="text-xs text-slate-500">
+            {`Written on ${format(new Date(createdAt), 'dd MMM yyyy')}`}
+          </p>
+        </CardFooter>
+      </Card>
+    </Skeleton>
   );
 };
 
