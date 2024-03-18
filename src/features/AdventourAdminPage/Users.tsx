b37d3358cc8fdd799ae99d9608474c6e4cf95a4e
Tushar <gaonkar.tushar01@gmail.com>
2024-03-18 17:52:22 +0530

    fix: removed redundant fragments

--- src/features/AdventourAdminPage/Users.tsx
+++ src/features/AdventourAdminPage/Users.tsx
@@ -1,12 +1,10 @@
 import UsersTable from './UsersTable';
 
-const Users = () => {
-  return (
-    <section className="flex flex-col gap-6">
-      <h1 className="text-2xl font-bold leading-relaxed text-primary">Registered Users</h1>
-      <UsersTable />
-    </section>
-  );
-};
+const Users = () => (
+  <section className="flex flex-col gap-6 p-2">
+    <h1 className="text-2xl font-bold leading-relaxed text-primary">Registered Users</h1>
+    <UsersTable />
+  </section>
+);
 
 export default Users;
