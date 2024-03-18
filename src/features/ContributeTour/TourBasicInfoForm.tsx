11d0f5166ff28ca0ac8e1629a49a7dbb3b5a091b
Tushar <gaonkar.tushar01@gmail.com>
2024-03-18 17:59:08 +0530

    refactor: moved "DropZone" component to common components for reusability

--- src/features/ContributeTour/TourBasicInfoForm.tsx
+++ src/features/ContributeTour/TourBasicInfoForm.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { Input, Textarea } from '@nextui-org/react';
 import { useCallback, useState } from 'react';
-import { DropzoneOptions, useDropzone } from 'react-dropzone';
+import { DropzoneOptions } from 'react-dropzone';
 import {
   UseFormRegister,
   FieldErrors,
@@ -11,44 +11,7 @@ import {
   useFieldArray,
 } from 'react-hook-form';
 import { type ContributeTourFormSchemaType } from '../../validators/ContributeTourFormValidator';
-
-const DropZone = ({ configuration }: { configuration: DropzoneOptions }) => {
-  const { getRootProps, getInputProps } = useDropzone(configuration);
-
-  return (
-    <div className="flex items-center justify-center w-full" {...getRootProps()}>
-      <label
-        htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-      >
-        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg
-            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
-          <p className="mb-2 text-sm text-gray-500 font-semibold">
-            Click to upload or drag and drop
-          </p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            PNG, JPG, JPEG, WebP (Max file size 3MB)
-          </p>
-        </div>
-        <input {...getInputProps()} />
-      </label>
-    </div>
-  );
-};
+import DropZone from '../../common/DropZone';
 
 const TourBasicInfoForm = ({
   register,
@@ -125,6 +88,7 @@ const TourBasicInfoForm = ({
         label="Tour Description"
         labelPlacement="outside"
         placeholder="Enter tour description"
+        disableAnimation
         {...register('description')}
         isInvalid={!!errors.description}
         errorMessage={errors.description?.message}
