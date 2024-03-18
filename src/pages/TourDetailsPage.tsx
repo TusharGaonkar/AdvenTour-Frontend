1f065d603a7c22b47d4c32f761d6c6511f0cf708
Tushar <gaonkar.tushar01@gmail.com>
2024-03-18 18:50:09 +0530

    feat: integrated similar tours , reviews to the tours page

--- src/pages/TourDetailsPage.tsx
+++ src/pages/TourDetailsPage.tsx
@@ -2,22 +2,36 @@
 // eslint-disable-next-line import/order
 import { useParams } from 'react-router-dom';
 import { Divider, Progress, Spinner } from '@nextui-org/react';
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 import { useGetIndividualTourInfoQuery } from '../redux/slices/getIndividualTourInfoSlice';
 import NavBar from '../common/Navbar';
 import 'react-photo-view/dist/react-photo-view.css';
 import TourTitle from '../features/TourDetails/TourTitle';
-import BookMarkTour from '../features/TourDetails/BookMarkTour';
 import RenderTourImages from '../features/TourDetails/RenderTourImages';
 import TourDescription from '../features/TourDetails/TourDescription';
 import TourQuickInfo from '../features/TourDetails/TourQuickInfo';
 import TourDetailedInfo from '../features/TourDetails/TourDetailedInfo';
+import { useGetSimilarToursQuery } from '../redux/slices/getSimilarToursSlice';
+import StatsProvider from '../common/StatsProvider';
+import CustomMobileNavigation from '../common/CustomMobileNavigation';
 import BookTour from '../features/TourDetails/BookTour';
 import Itinerary from '../features/TourDetails/Itinerary';
 import TourReviews from '../features/TourDetails/TourReviews';
+import MultiCarousel from '../features/TourDetails/MultiCarousel';
 
 const TourDetailsPage = () => {
   const { id } = useParams();
-  const { data: tourData, status, error } = useGetIndividualTourInfoQuery(id);
+  const { data: tourData, status, isError, error } = useGetIndividualTourInfoQuery(id);
+  const { data: similarToursData, isSuccess: isSimilarToursSuccess } = useGetSimilarToursQuery(id);
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error?.data?.message || 'Something went wrong...', {
+        className: 'font-medium text-xs',
+      });
+    }
+  }, [error?.data?.message, isError]);
 
   return (
     <>
@@ -25,20 +39,29 @@ const TourDetailsPage = () => {
         size="sm"
         isIndeterminate={status === 'pending'}
         aria-label="Loading..."
-        className="w-screen"
+        className="w-full overflow-hidden"
         color="danger"
       />
       <NavBar />
-      <div className="flex flex-col max-w-6xl mx-auto gap-7">
+
+      <div className="flex flex-col max-w-6xl mx-auto gap-7 overflow-clip">
         {status === 'fulfilled' && tourData.totalResults === 1 ? (
           <>
-            <div className="flex items-start justify-between">
-              <TourTitle tour={tourData?.data?.tour} />
-              <BookMarkTour />
+            <div className="flex md:flex-row flex-col items-start justify-between">
+              <StatsProvider tourID={id}>
+                {(statsData) => (
+                  <TourTitle
+                    tourTitle={tourData?.data?.tour?.title}
+                    createdBy={tourData?.data?.tour?.createdBy}
+                    statsData={statsData}
+                  />
+                )}
+              </StatsProvider>
             </div>
+
             <RenderTourImages tour={tourData.data.tour} />
-            <div className="grid items-start grid-cols-2 gap-2">
-              <div className="flex flex-col gap-7">
+            <div className="lg:grid lg:items-start lg:grid-cols-2 lg:gap-2">
+              <div className="flex flex-col gap-7 p-2">
                 <TourDescription tour={tourData?.data?.tour} />
                 <Divider />
                 <TourQuickInfo tour={tourData?.data?.tour} />
@@ -46,7 +69,7 @@ const TourDetailsPage = () => {
                 <TourDetailedInfo tour={tourData?.data?.tour} />
               </div>
 
-              <div className="sticky top-20">
+              <div className="sticky top-20 overflow-hidden">
                 <BookTour
                   tour={tourData?.data?.tour}
                   ageGroups={tourData?.data?.tour?.ageGroups}
@@ -55,9 +78,18 @@ const TourDetailsPage = () => {
               </div>
             </div>
             <Divider />
+
             <Itinerary tour={tourData?.data?.tour} />
+
             <Divider />
-            <TourReviews />
+            {isSimilarToursSuccess && (
+              <MultiCarousel similarTours={similarToursData?.data?.similarTours} />
+            )}
+            <Divider id="reviews" />
+
+            <TourReviews tourID={id as string} />
+
+            <CustomMobileNavigation />
           </>
         ) : (
           <div className="flex items-center justify-center h-[60vh]">
