35ac226ebf37f3ba8c004b2b869318d627a561e7
Tushar <gaonkar.tushar01@gmail.com>
2024-03-18 18:46:10 +0530

    feat: Protected route HOC  for admin routes

--- src/pages/ProtectedAdminRoute.tsx
+++ src/pages/ProtectedAdminRoute.tsx
@@ -1,9 +1,10 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
 
 const ProtectedAdminRoute = () => {
   const redirectURL = window.location.pathname;
-  const { isLoggedIn, user } = useSelector((state: any) => state.userInfo);
+  const { isLoggedIn, user } = useSelector((state: RootState) => state.userInfo);
 
   if (isLoggedIn && user?.role === 'admin') {
     return <Outlet />;
