855341dcc941b16aa1d4beb7e7de95432e74470a
Tushar <gaonkar.tushar01@gmail.com>
2024-03-18 18:29:35 +0530

    fix: fix the geo-location alerts showing multiple times

--- src/features/Tours/NearByTours.tsx
+++ src/features/Tours/NearByTours.tsx
@@ -1,30 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Switch } from '@nextui-org/react';
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
 import { isMobile } from 'react-device-detect';
+import { RootState } from '../../app/store';
 
 const NearByTours = ({
   setCoords,
 }: {
   setCoords: React.Dispatch<React.SetStateAction<number[] | null>>;
 }) => {
-  const { getNearbyTours } = useSelector((state) => state.filterToursQueryString);
+  const { getNearbyTours } = useSelector((state: RootState) => state.filterToursQueryString);
   const [isSelected, setIsSelected] = useState<boolean>(!!getNearbyTours);
+
+  const isNotified = useRef(false);
   useEffect(() => {
     if (isSelected) {
       if (navigator.geolocation) {
+        let toastID: string | null = null;
+        if (!isNotified.current) {
+          toastID = toast.loading('Detecting your current location...', {
+            className: 'font-medium text-xs',
+          });
+        }
+
         navigator.geolocation.getCurrentPosition(
           (position) => {
-            if (!isMobile) {
+            if (isSelected && !isMobile && !isNotified.current) {
               toast.success(
                 'Please note that the detected location can be inaccurate due to limited access to GPS on this device!',
-                { className: 'font-medium text-xs rounded-xl', duration: 4000 }
+                { className: 'font-medium text-xs' }
               );
+              isNotified.current = true;
             }
+            toast.dismiss(toastID);
             setCoords([position.coords.longitude, position.coords.latitude]);
           },
           (error) => {
+            toast.dismiss(toastID);
             setIsSelected(false);
             setCoords(null);
             toast.error(`${error.message}, please reset the permission`, {
@@ -45,7 +58,7 @@ const NearByTours = ({
 
   return (
     <div className="flex items-center self-stretch justify-between w-full p-3 bg-gray-100 rounded-xl">
-      <p className="text-xs font-semibold"> Get nearby tours</p>
+      <p className="text-xs font-semibold tracking-wide"> Get nearby tours</p>
       <Switch
         className="h-full"
         isSelected={isSelected}
