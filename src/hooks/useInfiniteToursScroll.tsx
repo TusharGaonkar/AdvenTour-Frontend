93e9584b3db40d5b747ac1cb21d01cc33b75c20a
Tushar <gaonkar.tushar01@gmail.com>
2024-04-01 18:25:33 +0530

    feat: custom infinite tours scroll using RTK query

--- src/hooks/useInfiniteToursScroll.tsx
+++ src/hooks/useInfiniteToursScroll.tsx
@@ -1,3 +1,4 @@
+// Custom implementation of infinite scroll hook for tours using RTK query
 import { useCallback, useRef, useState, useEffect } from 'react';
 import { useGetAllToursQuery } from '../redux/slices/getToursSlice';
 
@@ -5,36 +6,47 @@ type TourDataType = Record<string, any>;
 
 export default function useInfiniteToursScroll(queryString: string) {
   const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(3);
+  const [limit] = useState(3); // Limit doesn't need to be stateful unless it changes dynamically
   const [hasMoreData, setHasMoreData] = useState(true);
-  const [toursData, setToursData] = useState<TourDataType>([]);
+  const [noResultsFound, setNoResultsFound] = useState(false);
+  const [toursData, setToursData] = useState<TourDataType[]>([]);
 
-  const { data, isSuccess, isLoading, isError, error } = useGetAllToursQuery({
+  const { data, isSuccess, isLoading, isFetching, isError, error, status } = useGetAllToursQuery({
     queryString,
     page,
     limit,
   });
 
   useEffect(() => {
-    if (isSuccess && data) {
-      console.log('called', page);
-      setToursData((prevTours: TourDataType[]) => [
-        ...prevTours,
-        ...(data.data?.tours as TourDataType[]),
-      ]);
+    setToursData([]);
+    setHasMoreData(true);
+    setNoResultsFound(false);
+    setPage(1);
+  }, [queryString]); // Reset the page when the query string changes
 
-      setHasMoreData((data.totalResults || 0) > 0);
+  useEffect(() => {
+    const scrollPosition = window.scrollY;
+    if (data && status === 'fulfilled') {
+      const newTourData = data.data?.tours || [];
+      if (newTourData.length === 0 && page === 1) {
+        setNoResultsFound(true);
+        return;
+      }
+      setToursData((prevTours: TourDataType[]) => [...prevTours, ...newTourData]);
+      const isEnd = (data.totalResults || 0) < limit;
+      setHasMoreData(!isEnd);
     }
-  }, [page, isSuccess, data]);
+    window.scrollTo(0, scrollPosition); // prevent scrolling to top when new data is fetched
+  }, [data, limit, page, status]);
 
-  const observerRef = useRef<IntersectionObserver>();
+  const observerRef = useRef<IntersectionObserver | null>(null);
 
   const handleScrollToBottom = useCallback(
-    (node: HTMLElement) => {
+    (node: HTMLElement | null) => {
       if (observerRef.current) {
         observerRef.current.disconnect(); // remove all previous observers
       }
-      if (hasMoreData && !isLoading && !isError) {
+      if (node && hasMoreData && !isFetching) {
         observerRef.current = new IntersectionObserver((entries) => {
           const entry = entries[0];
           if (entry.isIntersecting) {
@@ -42,11 +54,23 @@ export default function useInfiniteToursScroll(queryString: string) {
           }
         });
 
-        if (node) observerRef.current.observe(node);
+        observerRef.current.observe(node);
       }
     },
-    [isLoading, isError, hasMoreData]
+    [hasMoreData, isFetching]
   );
 
-  return [toursData, { page, isLoading, isError, error, isSuccess, handleScrollToBottom }];
+  return [
+    toursData,
+    {
+      page,
+      isSuccess,
+      isLoading,
+      isFetching,
+      isError,
+      error,
+      handleScrollToBottom,
+      noResultsFound,
+    },
+  ];
 }
