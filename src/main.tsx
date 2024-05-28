6fc6c50f0bd8eb7dbc5eaf73935ccaea3c55fd70
Tushar <gaonkar.tushar01@gmail.com>
2024-05-28 10:57:17 +0530

    feat: enable Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import { Provider } from 'react-redux';
 import App from './app/App';
 import store from './app/store';
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <App />
       </BrowserRouter>
     </Provider>
+    <Analytics />
     <SpeedInsights />
   </React.StrictMode>
 );
